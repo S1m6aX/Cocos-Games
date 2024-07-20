a7852ff722b944268263a0928eb0dd59b66ad477
simba0913 <simbax0913@gmail.com>
2024-07-20 15:39:24 +0800

    Adding collison with enemy cause player die

--- Plane War/assets/Script/PlayerControl.ts
+++ Plane War/assets/Script/PlayerControl.ts
@@ -1,27 +1,55 @@
+import EnemyControl from "./EnemyControl";
+
 const { ccclass, property } = cc._decorator;
 
 @ccclass
 export default class PlayerControl extends cc.Component {
   @property(cc.Prefab)
   bulletPre: cc.Prefab = null;
+  isDie: boolean = false;
 
   start() {
-    // 移动
-    this.node.on(cc.Node.EventType.TOUCH_MOVE, (event) => {
-      this.node.setPosition(event.getLocation());
-    });
-    // 攻击 计时器
-    this.schedule(() => {
-      // 创建子弹
-      let bullet = cc.instantiate(this.bulletPre);
-      // 设置父物体
-      bullet.setParent(cc.director.getScene());
-      // 设置位置
-      bullet.x = this.node.x;
-      bullet.y = this.node.y + 70;
-    }, 0.5);
+    if (!this.isDie) {
+      // 移动
+      this.node.on(cc.Node.EventType.TOUCH_MOVE, (event) => {
+        this.node.setPosition(event.getLocation());
+      });
+      // 攻击 计时器
+      this.schedule(this.shoot, 0.5);
+    }
+
     // 开启碰撞检测
     cc.director.getCollisionManager().enabled = true;
+  }
+
+  shoot() {
+    // 创建子弹
+    let bullet = cc.instantiate(this.bulletPre);
+    // 设置父物体
+    bullet.setParent(cc.director.getScene());
+    // 设置位置
+    bullet.x = this.node.x;
+    bullet.y = this.node.y + 70;
+  }
+
+  onCollisionEnter(other) {
+    // 如果碰到敌人，双双销毁
+    if (other.tag == 1) {
+      this.die();
+      other.getComponent(EnemyControl).die();
+    }
+  }
+
+  die() {
+    this.isDie = true;
+
+    cc.loader.loadRes("hero1_die", cc.SpriteFrame, (err, res) => {
+      this.node.getComponent(cc.Sprite).spriteFrame = res;
+    });
+
+    setTimeout(() => {
+      this.node.destroy();
+    }, 300);
   }
 
   update() {}
